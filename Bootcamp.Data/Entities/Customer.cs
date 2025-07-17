8ddc4462c5f489d10ac1fe7c46c965c14d6a97e8
ingersoll-brendan <brendaningersoll136@gmail.com>
2025-07-17 10:42:13 -0700

    Modified customer form and SCSS support

--- Bootcamp.Data/Entities/Customer.cs
+++ Bootcamp.Data/Entities/Customer.cs
@@ -5,48 +5,53 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Bootcamp.Data.Entities
 {
-    public class Customer
-    {
-        #region ID/Foreign Key Properties
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+	public class Customer
+	{
+		#region ID/Foreign Key Properties
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
 
-        [Required]
-        public int[] AddressIds { get; set; } = [];
+		[Required]
+		public int[] AddressIds { get; set; } = [];
 
-        [Required]
-        public int[] OrderIds { get; set; } = [];
+		[Required]
+		public int[] OrderIds { get; set; } = [];
 
-        #endregion
+		#endregion
 
-        #region Attribute Properties
+		#region Attribute Properties
 
-        [Required]
-        [MaxLength(50)]
-        public string? FirstName { get; set; }
+		[Required(ErrorMessage = "First Name is Required")]
+		[MaxLength(50, ErrorMessage = "First Name cannot be longer than 50 characters")]
+		public string? FirstName { get; set; }
 
-        [Required]
-        [MaxLength(50)]
-        public string? LastName { get; set; }
+		[Required(ErrorMessage = "Last Name is Required")]
+		[MaxLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters")]
+		public string? LastName { get; set; }
 
-        [Required]
-        [MaxLength(255)]
-        public string? Email { get; set; }
+		[Required(ErrorMessage = "Email is Required")]
+		[MaxLength(255, ErrorMessage = "Email cannot be longer than 255 characters")]
+		[RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")]
+		//TODO: Add regular expressions for email
+		public string? Email { get; set; }
 
-        [Required]
-        public long PhoneNumber {  get; set; }
 
-        #endregion
+		// TODO: Change PhoneNumber to string and add validation for phone number format using Regex
+		[Required(ErrorMessage = "Phone Number is Required")]
+		public long PhoneNumber { get; set; }
 
-        #region Navigation Properties
+		#endregion
 
-        public ICollection<Address> Addresses { get; set; } = new List<Address>();
-        public ICollection<Order> Orders { get; set; } = new List<Order>();
+		#region Navigation Properties
 
-        #endregion
-    }
+		public ICollection<Address> Addresses { get; set; } = new List<Address>();
+		public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+		#endregion
+	}
 }
