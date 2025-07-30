a8b899af166a8c1f52d0b6e8665e611384e5f0ab
ingersoll-brendan <brendaningersoll136@gmail.com>
2025-07-30 11:36:17 -0700

    Added migration for database AddressTypes

---
+++ Bootcamp.Data/Migrations/20250730182808_BootcampMigrationUpdate9.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bootcamp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BootcampMigrationUpdate9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AddressTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Billing" },
+                    { 2, "Shipping" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AddressTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AddressTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
