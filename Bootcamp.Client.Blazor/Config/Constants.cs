a8c90f72d0746c8cb457df80ed9f283159fe2026
ingersoll-brendan <brendaningersoll136@gmail.com>
2025-07-31 12:58:58 -0700

    Changes to YAML to build an output for each Build Config

--- Bootcamp.Client.Blazor/Config/Constants.cs
+++ Bootcamp.Client.Blazor/Config/Constants.cs
@@ -2,31 +2,40 @@
 
 public static class Constants
 {
-    #region Bootcamp API Config
+	#region Bootcamp API Config
 
-    public const string Bootcamp_ApiHttpClientName = "BootcampAPI";
+	public const string Bootcamp_ApiHttpClientName = "BootcampAPI";
 
-    // TODO: We need to determine how to configure this for all environments?
+	// TODO: We need to determine how to configure this for all environments?
 
-    // TODO: Use a "Compiler" Statement (i.e. #if DEV, #if INT, #if PROD, etc.
-    // Or does Blazor have AppSettings.json that can be used simaliar to Web Projects
-    // and an run time, we can load the correct ones?
-    public const string Bootcamp_ApiBaseUrl = "https://localhost:7119/api/";
+	// TODO: Use a "Compiler" Statement (i.e. #if DEV, #if INT, #if PROD, etc.
+	// Or does Blazor have AppSettings.json that can be used simaliar to Web Projects
+	// and an run time, we can load the correct ones?
 
-    #endregion
+#if DEBUG || DEV
+		public const string Bootcamp_ApiBaseUrl = "https://localhost:7119/api/";
+#elif INT
+		public const string Bootcamp_ApiBaseUrl = "https://symmsoft-bootcamp-api-int.azurewebsites.net/api/";
 
-    #region Bootcamp API Endpoints
+		// FUTURE: Add other Environment URLs (i.e. QA, UAT)
+#else
+	public const string Bootcamp_ApiBaseUrl = "https://symmsoft-bootcamp-api-prod.azurewebsites.net/api";
+	#endif
 
-    public const string Bootcamp_Customers = "customers";
-    public const string Bootcamp_Customer = "customers/{id}";
+	#endregion
 
-    public const string Bootcamp_Addresses = "addresses";
-    public const string Bootcamp_Address = "addresses/{id}";
-    public const string Bootcamp_AddressesByCustomerId = "addresses/customer/{id}";
+	#region Bootcamp API Endpoints
 
-    public const string Bootcamp_Orders = "orders";
-    public const string Bootcamp_Order = "orders/{id}";
-    public const string Bootcamp_OrdersByCustomerId = "orders/customer/{id}";
+	public const string Bootcamp_Customers = "customers";
+	public const string Bootcamp_Customer = "customers/{id}";
+
+	public const string Bootcamp_Addresses = "addresses";
+	public const string Bootcamp_Address = "addresses/{id}";
+	public const string Bootcamp_AddressesByCustomerId = "addresses/customer/{id}";
+
+	public const string Bootcamp_Orders = "orders";
+	public const string Bootcamp_Order = "orders/{id}";
+	public const string Bootcamp_OrdersByCustomerId = "orders/customer/{id}";
 
 	#endregion
 }
